187303d03198333052ac02030d5d4acd900f16fa
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-06-22 15:55:33 -0300

    enviando build do RGBModulos

---
+++ precache-manifest.71b7e103cd5075b774d273b8ffb6c5f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719669dd587f1cc40f759b1ae4990856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da95327aced243ef56a",
+    "url": "/static/js/2.02822e19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02822e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd6a3a4749face4a85b",
+    "url": "/static/js/main.4efe7cc4.chunk.js"
+  },
+  {
+    "revision": "e166dc5b6bda3e612ef4",
+    "url": "/static/js/runtime-main.fe62ce3a.js"
+  }
+]);
